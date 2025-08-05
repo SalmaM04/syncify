7bd79be137aa815240bf56f70f23ffebd75394fa
Salma Mohammad <salmamoh5221@gmail.com>
2025-08-04 18:24:39 -0700

    Initial Commit

---
+++ syncify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  syncify
+//
+//  Created by Salma M. on 8/4/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ syncifyTests/syncifyTests.swift
@@ -0,0 +1,17 @@
+//
+//  syncifyTests.swift
+//  syncifyTests
+//
+//  Created by Salma M. on 8/4/25.
+//
+
+import Testing
+@testable import syncify
+
+struct syncifyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
